cf6c49591bc618b839340a0c94509a82a5fc0913
darunshen <465530157@qq.com>
2020-07-01 21:39:18 +0800

    add a tmp code dir 'creek_tmp' for creek

--- creek/include/generated/compile.h
+++
@@ -1,6 +0,0 @@
-/* This file is auto generated, version 0 */
-#define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#0 Sat Jun 20 18:16:30 CST 2020"
-#define LINUX_COMPILE_BY "bigrain"
-#define LINUX_COMPILE_HOST "bigrain-Lenovo-G500"
-#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04) "
