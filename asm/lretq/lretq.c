62974414e1982fc897603c643954dfda7a345263
run.tian@senscape.com.cn <run.tian@senscape.com.cn>
2020-11-02 15:28:57 +0800

    add asm demo

---
+++ asm/lretq/lretq.c
@@ -0,0 +1,15 @@
+/*
+ * Created on Wed Oct 21 2020
+ * Author : tianrun
+ * Copyright (c) 2020
+ */
+#include <stdio.h>
+void print() {
+  while (0) {
+  }
+}
+// int main(int argc, char const *argv[])
+// {
+//     print();
+//     return 0;
+// }
